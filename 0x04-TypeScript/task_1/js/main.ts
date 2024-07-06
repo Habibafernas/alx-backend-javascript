2a36f1e5ce4e7f90500827b7bd7df0a3ed7d12f1
habibafernas <habibafernas@gmail>
2024-07-06 21:44:23 +0300

    updating

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number;
+  location: string;
+  contract: boolean;
+  [index:string]: any;
+}
